312d4a3a08ca9eb8275ee76dc899b59082136605
Pat <oalyssapatricia@gmail.com>
2023-10-04 22:39:33 +0800

    chore: rechecking

--- doc/conf.py
+++ doc/conf.py
@@ -14,18 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-
-              # Add the 'sphinxcontrib.httpdomain' extension to your extensions list
-extensions = ['myst_parser',
-              'sphinxcontrib-swaggerdoc',
-              'sphinxcontrib-httpdomain']  # Include the 'sphinxcontrib.httpdomain' extension
-
-# Add the path to your Swagger JSON file as a configuration option
-swaggerdoc_api_spec = 'https://github.com/alyssapatricia/ui/blob/main/doc/ehub-backend/api-docs/ehub-backend-api-docs.json'
-
-# Set the base URL for your Swagger documentation (GitHub Pages URL)
-swaggerdoc_basepath = '/ui/'  # Replace '<repository-name>' with your GitHub repository name
-
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']

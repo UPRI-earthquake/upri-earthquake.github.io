7bc63c371341d41f5e00940758d87697ec8362d1
Pat <oalyssapatricia@gmail.com>
2023-11-10 20:44:24 +0800

    test: uploading diagrams

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser','sphinxcontrib.redoc']
+extensions = ['myst_parser','sphinxcontrib.openapi','sphinxcontrib.redoc']
 redoc = [
     {
         'name': 'UPRI API',

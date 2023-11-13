1a8d3bd87909e20cde288e4ef0eed1d33516f9d5
Pat <oalyssapatricia@gmail.com>
2023-11-13 20:09:16 +0800

    test: api deployment

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 html_theme_options = {
     "light_css_variables": {
